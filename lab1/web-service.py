caf255855feeaaa9eb80bc5d9fdfb2363621066c
John L. Whiteman <>
2024-01-13 14:01:13 -0800

    Added sample web service
    Signed-off-by: John L. Whiteman <>

---
+++ lab1/web-service.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_squirrel():
+    return "<p>Hello, Squirrel!</p>"
+
+@app.route("/encode", methods=['GET'])
+def encode():
+  text = request.args.get('text')
+  # DO YOUR ENCODE WORK HERE
+  ciphertext = "your encoded codebook stuff goes here"
+  return jsonify({"plaintext": text,
+                  "ciphertext": ciphertext,
+                  "operation": "encode"})
+
+@app.route("/decode", methods=['GET'])
+def decode():
+  text = request.args.get('text')
+  # DO YOUR DECODE WORK HERE
+  plaintext = "your decoded codebook stuff goes here"
+  return jsonify({"plaintext": plaintext,
+                  "ciphertext": text,
+                  "operation": "encode"})
+
+if __name__ == "__main__":
+    app.run()
